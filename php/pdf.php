5468709a313f280223a9815709c9aaec1d596b74
eri <a22310383@ceti.mx>
2024-05-03 17:49:52 -0600

    pdf pt 2 (w products)

--- php/pdf.php
+++ php/pdf.php
@@ -54,16 +54,68 @@
     //Datos del comprador
     $pdf->SetXY(25,50);
     $pdf->Cell(60,10,'PARA:');
-    $pdf->Cell(60,10,'Email:');
+    $pdf->Cell(60,10,'EMAIL:');
     $pdf->SetXY(25,53);
     $pdf->SetTextColor(131,164,102);
-    $pdf->Cell(60,15,$rUsr['nombre'].' '.$rUsr['apellido']);
+    $pdf->Cell(60,15, $rUsr['nombre'].' '.$rUsr['apellido']);
     $pdf->SetTextColor(0,0,0);
-    $pdf->Cell(60,15,$rUsr['correo']);
+    $pdf->Cell(60,15, $rUsr['correo']);
     $pdf->SetXY(25,56);
 
-
+    // Datos de la compra
     $y = 40;
+    // $pdf->SetDrawColor(0,0,0):
+    $pdf->SetDrawColor(131,164,102);
+    $pdf->SetFillColor(131,164,102);
+
+    $pdf->SetXY(25,85);
+    $pdf->Cell(40,10,'Modelo de maceta',1,0,'c', true);
+    $pdf->Cell(20,10,'Precio ',1,0,'c', true);
+    $pdf->Cell(20,10,'Cantidad',1,0,'c', true);
+    $pdf->Cell(20,10,'IVA',1,0,'c', true);
+    $pdf->Cell(25,10,'Subtotal',1,1,'c', true);
+
+    $total = 0;
+
+    // Verificar si el carrito está definido
+    if (isset($_SESSION['carrito'])) {
+        $pdf->SetXY(25, 95);
+        foreach ($_SESSION['carrito'] as $btn_sku => $maceta) {
+            $pdf->SetX(25);
+            $pdf->Cell(40, 5, $maceta['modelo'], 1, 0, "L");
+            $pdf->Cell(20, 5, "$" . $maceta['precio'], 1, 0, "L");
+            $pdf->Cell(20, 5, $maceta['cantidad'], 1, 0, "L");
+
+            $iva = $maceta['subtotal'] * 0.16;
+
+            $pdf->Cell(20, 5, "$" . $iva, 1, 0, "L");
+            $pdf->Cell(25, 5, "$" . $maceta['subtotal'], 1, 1, "L");
+
+            $total += $maceta['subtotal'] + $iva;
+        }
+
+        $IVA = $total * 0.16;
+        $sub = $total + $IVA;
+
+        $pdf->SetXY(25, 170);
+        $pdf->Cell(30, 5, "TOTAL:", 0, "L");
+        $pdf->SetXY(50, 170);
+        $pdf->Cell(30, 5, " $ " . $total, "", 0, "L");
+
+        $pdf->SetXY(25, 175);
+        $pdf->Cell(30, 5, "IVA:", 0, "L");
+        $pdf->SetXY(50, 175);
+        $pdf->Cell(30, 5, " $ " . $IVA, "", 0, "L");
+
+        $pdf->SetXY(25, 180);
+        $pdf->Cell(30, 5, "SUBTOTAL:", 0, "L");
+        $pdf->SetXY(50, 180);
+        $pdf->Cell(30, 5, " $ " . $sub, "", 0, "L");
+    } else {
+        $pdf->SetXY(25, 95);
+        $pdf->Cell(0, 10, "El carrito se encuentra vacio en estos momentos", 0, 1);
+    }
+
 
 
     //footer
