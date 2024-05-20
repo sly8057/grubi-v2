1ef13f075f600fcd7cbfc01f5a5114becea16471
eri <a22310383@ceti.mx>
2024-05-19 20:46:18 -0600

    remove unnecessary folder

--- php/ejemplos/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_destroy();
-$msg = "Sesion destruida ";
-header("refresh:1; url=form.html");
-            echo '<div>'.$msg.'</div>';
-            echo '<p>Serás redirigido al índice en 5 segundos.</p>';
-?>
