f12eff711c3ea7c2530f076d05ba14a878f3cd4a
eri <a22310383@ceti.mx>
2024-06-17 13:50:38 -0600

    little changes

--- connection.php
+++ connection.php
@@ -11,22 +11,3 @@
     //     echo "Conexión exitosa";
     }
 ?>
-
-<?php
-// class Connection {
-//     public static function connect() {
-
-//         $con = new PDO(
-//             "mysql:host=localhost;dbname=grubi-v2",
-//             "root",
-//             "",
-//             array(
-//                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-//                 PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
-//             )
-//         );
-
-//         return $con;
-//     }
-// }
-?>
